a07110f7967e74bbb4732b92ad8e9af78c4b34eb
sunday0prime <sundayprime@gmail.com>
2022-09-15 12:26:10 -0700

    C program debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* larget_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+
+int main(void)
+{
+    int largest;
+    if (a >= b && a >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && b >= c)
+    {
+        largest = b
+    }
+    else
+    {
+        largest = c
+    }
+    return (largest);
+}

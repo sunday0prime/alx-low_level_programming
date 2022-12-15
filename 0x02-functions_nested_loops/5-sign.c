bb28e151678f51622d07d40f3b70b37c07053671
sunday0prime <sundayprime@gmail.com>
2022-12-15 14:09:51 -0800

    no commit message

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,16 +5,20 @@
 * Return: 0
 */
 
-int print_sign(int n) {
-	if (n>0) {
+int print_sign(int n)
+{
+	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0) {
-		_putchar('-')
+	else if (n < 0)
+	{
+		_putchar('-');
 		return (-1);
 	}
-	else {
+	else
+	{
 		_putchar('0');
 		return (0);
 	}
